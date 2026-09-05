2522e51ffdd5b6140926398221198ba250dd71e8
Dilip <dilip.suthar@neosoftmail.com>
2026-09-05 19:42:57 +0530

    refactor: added react-native-fieldops-ui preset in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/app/index.tsx", "./src/components/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  presets: [
+    require("nativewind/preset"),
+    require("react-native-fieldops-ui/preset"),
+  ],
   theme: {
     extend: {},
   },
